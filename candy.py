26c4b92531fb6042b36f7ce2e28dae50073a80a3
Goro <evilninjax@gmail.com>
2019-01-13 14:24:33 -0500

    base todo

--- candy.py
+++ candy.py
@@ -10,3 +10,6 @@ We want to find some results of the candy sales data
 # TODO: determine how many of each flavor sold
 
 # TODO: determine who bought the most of each flavor
+
+
+# TODO: COMING SOON, another question/determination to make!
