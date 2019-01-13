e331016b903ce9c0ef027610522989230aa59aa4
Goro <evilninjax@gmail.com>
2019-01-13 14:22:57 -0500

    base data

---
+++ candy.py
@@ -0,0 +1,12 @@
+"""
+Candy data reader
+We want to find some results of the candy sales data
+
+"""
+
+
+# TODO: read in from transactions.csv AND items.csv
+
+# TODO: determine how many of each flavor sold
+
+# TODO: determine who bought the most of each flavor
